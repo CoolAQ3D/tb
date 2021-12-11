fe3017fd5bca61d17d72aced12ed9f12e237bd5f
Jack Soni <95261764+CoolAQ3D@users.noreply.github.com>
2021-12-11 01:10:49 +0000

    2

--- main.sh
+++ main.sh
@@ -1,22 +1,14 @@
 #Install Toolbox v3
 if [[ ! -d Toolbox ]];then
   echo 'Downloading Library!!!'
+  git clone https://github.com/CoolAQ3D/tb Toolbox
 else
   #Check if toolbox exist
-  python Toolbox/replit/install_toolbox.py
+  python Toolbox/Toolbox/replit/install_toolbox.py
   #Check for update
-  sh Toolbox/replit/update.sh
+  sh Toolbox/Toolbox/replit/update.sh
 fi
 
 
-
-
-
-
-
-
-
-
-
-
+#python main.py
 
